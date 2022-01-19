55fefaa685cb73ebfe3354c9f616d0b9fde039de
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2022-01-19 10:08:58 +0200

    Add new task

---
+++ HackerRankProblems/03.Mini-MaxSum/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace Mini_MaxSum
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'miniMaxSum' function below.
+         *
+         * The function accepts INTEGER_ARRAY arr as parameter.
+         */
+
+        public static void miniMaxSum(List<int> arr)
+        {
+            var listOfSums = new List<Int64>();
+
+            for (var i = 0; i < arr.Count; i++)
+            {
+                Int64 sum = 0;
+
+                foreach (var item in arr)
+                {
+                    sum += item;
+                }
+
+                sum -= arr[i];
+                listOfSums.Add(sum);
+            }
+
+            var minSum = listOfSums.Min();
+            var maxSum = listOfSums.Max();
+
+            Console.WriteLine($"{minSum} {maxSum}");
+        }
+    }
+
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            Result.miniMaxSum(arr);
+        }
+    }
+}
