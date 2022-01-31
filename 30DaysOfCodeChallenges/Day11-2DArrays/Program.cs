a9b692a879470cd8f39c11c164c3615d00aed108
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2022-01-31 11:13:44 +0200

    Day11

---
+++ 30DaysOfCodeChallenges/Day11-2DArrays/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day11_2DArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<List<int>> arr = new List<List<int>>();
+
+            for (int i = 0; i < 6; i++)
+            {
+                arr.Add(Console.ReadLine()
+                               .TrimEnd()
+                               .Split(' ')
+                               .ToList()
+                               .Select(arrTemp => Convert.ToInt32(arrTemp))
+                               .ToList());
+            }
+
+            int max_sum = int.MinValue;
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+
+                    int temp_sum = 0;
+                    // top row
+                    temp_sum += arr[i][j];
+                    temp_sum += arr[i][j + 1];
+                    temp_sum += arr[i][j + 2];
+                    //middle 
+                    temp_sum += arr[i + 1][j + 1];
+                    //bottom row
+                    temp_sum += arr[i + 2][j];
+                    temp_sum += arr[i + 2][j + 1];
+                    temp_sum += arr[i + 2][j + 2];
+
+                    //if first hourglass, set as max
+                    if (temp_sum > max_sum)
+                    {
+                        max_sum = temp_sum;///this will keep it up to date
+                    }
+                }
+            }
+
+            Console.WriteLine(max_sum);
+        }
+    }
+}
+/*Context
+Given a 6 * 6 2D Array, A:
+
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+
+We define an hourglass in A to be a subset of values with indices falling in this pattern in A's graphical representation:
+
+a b c
+  d
+e f g
+
+There are 16 hourglasses in A, and an hourglass sum is the sum of an hourglass' values.
+
+Task
+Calculate the hourglass sum for every hourglass in , then print the maximum hourglass sum.
+
+Example
+
+In the array shown above, the maximum hourglass sum is 7 for the hourglass in the top left corner.
+
+Input Format
+
+There are 6 lines of input, where each line contains 6 space-separated integers that describe the 2D Array A.
+
+Output Format
+
+Print the maximum hourglass sum in A.
+
+Sample Input
+
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 2 4 4 0
+0 0 0 2 0 0
+0 0 1 2 4 0
+
+Sample Output
+19
+
+Explanation
+
+A contains the following hourglasses:
+
+1 1 1   1 1 0   1 0 0   0 0 0
+  1       0       0       0
+1 1 1   1 1 0   1 0 0   0 0 0
+
+0 1 0   1 0 0   0 0 0   0 0 0
+  1       1       0       0
+0 0 2   0 2 4   2 4 4   4 4 0
+
+1 1 1   1 1 0   1 0 0   0 0 0
+  0       2       4       4
+0 0 0   0 0 2   0 2 0   2 0 0
+
+0 0 2   0 2 4   2 4 4   4 4 0
+  0       0       2       0
+0 0 1   0 1 2   1 2 4   2 4 0
+
+The hourglass with the maximum sum (19) is:
+
+2 4 4
+  2
+1 2 4
+ */
