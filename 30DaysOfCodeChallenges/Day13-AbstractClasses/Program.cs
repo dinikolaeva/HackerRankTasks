5799053204be6543688a1f6eb234c1fcc44aa8d6
Dilyana Nikolaeva <76437069+dinikolaeva@users.noreply.github.com>
2022-02-02 11:59:18 +0200

    Day13

---
+++ 30DaysOfCodeChallenges/Day13-AbstractClasses/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace Day13_AbstractClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String title = Console.ReadLine();
+            String author = Console.ReadLine();
+            int price = Int32.Parse(Console.ReadLine());
+            Book new_novel = new MyBook(title, author, price);
+            new_novel.display();
+        }
+    }
+}
+
+abstract class Book
+{
+
+    protected String title;
+    protected String author;
+
+    public Book(String t, String a)
+    {
+        title = t;
+        author = a;
+    }
+
+    public abstract void display();
+}
+
+class MyBook : Book
+{
+    protected int price;
+
+    public MyBook(string title, string author, int price) 
+        : base(title, author)
+    {
+        this.price = price;
+    }
+
+    public override void display()
+    {
+        Console.WriteLine($"Title: {this.title}\nAuthor: {this.author}\nPrice: {this.price}");
+    }
+}
+
+/*Task
+Given a Book class and a Solution class, write a MyBook class that does the following:
+
+Inherits from Book
+
+Has a parameterized constructor taking these 3 parameters:
+
+1. string title
+2. string author
+3. int price
+
+Implements the Book class' abstract display() method so it prints these 3 lines:
+
+1. Title:, a space, and then the current instance's title.
+2. Author:, a space, and then the current instance's author.
+3. Price:, a space, and then the current instance's price.
+
+Note: Because these classes are being written in the same file, you must not use an access modifier (e.g.: ) when declaring MyBook or your code will not execute.
+
+Input Format
+
+You are not responsible for reading any input from stdin. The Solution class creates a Book object and calls the MyBook class constructor (passing it the necessary arguments). It then calls the display method on the Book object.
+
+Output Format
+
+The void display() method should print and label the respective title, author, and price of the MyBook object's instance (with each value on its own line) like so:
+
+Title: $title
+Author: $author
+Price: $price
+
+Note: The $ is prepended to variable names to indicate they are placeholders for variables.
+
+Sample Input
+
+The following input from stdin is handled by the locked stub code in your editor:
+
+The Alchemist
+Paulo Coelho
+248
+
+Sample Output
+
+The following output is printed by your display() method:
+
+Title: The Alchemist
+Author: Paulo Coelho
+Price: 248
+ */
